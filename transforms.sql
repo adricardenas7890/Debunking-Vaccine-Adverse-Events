0b597605a776b77766ac2db15860f5aba7379800
Adriana Cardenas <adri.cardenas@utexas.edu>
2019-02-28 19:45:02 -0600

    transforms.sql updated

--- transforms.sql
+++ transforms.sql
@@ -62,59 +62,78 @@ where symptom1 is not null and symptom2 is not null
 
 --Table Symptom
 --union of all unique symptoms for each vaers_id
+create table dataset1clean.vaersid_symptom as
 select distinct vaers_id, symptom1 symptom
 from dataset1.symptoms2006
+where symptom1 is not null
 union distinct
 select distinct vaers_id, symptom2 symptom
 from dataset1.symptoms2006
+where symptom2 is not null
 union distinct 
 select distinct vaers_id, symptom1 symptom
 from dataset1.symptoms2007
+where symptom1 is not null
 union distinct
 select distinct vaers_id, symptom2 symptom
 from dataset1.symptoms2007
+where symptom2 is not null
 union distinct
 select distinct vaers_id, symptom1 symptom
 from dataset1.symptoms2008
+where symptom1 is not null
 union distinct
 select distinct vaers_id, symptom2 symptom
 from dataset1.symptoms2008
+where symptom2 is not null
 union distinct
 select distinct vaers_id, symptom1 symptom
 from dataset1.symptoms2009
+where symptom1 is not null
 union distinct
 select distinct vaers_id, symptom2 symptom
 from dataset1.symptoms2009
+where symptom2 is not null
 union distinct
 select distinct vaers_id, symptom1 symptom
 from dataset1.symptoms2010
+where symptom1 is not null
 union distinct
 select distinct vaers_id, symptom2 symptom
 from dataset1.symptoms2010
+where symptom2 is not null
 union distinct
 select distinct vaers_id, symptom1 symptom
 from dataset1.symptoms2011
+where symptom1 is not null
 union distinct
 select distinct vaers_id, symptom2 symptom
 from dataset1.symptoms2011
+where symptom2 is not null
 union distinct
 select distinct vaers_id, symptom1 symptom
 from dataset1.symptoms2012
+where symptom1 is not null
 union distinct
 select distinct vaers_id, symptom2 symptom
 from dataset1.symptoms2012
+where symptom2 is not null
 union distinct
 select distinct vaers_id, symptom1 symptom
 from dataset1.symptoms2013
+where symptom1 is not null
 union distinct
 select distinct vaers_id, symptom2 symptom
 from dataset1.symptoms2013
+where symptom2 is not null
 union distinct
 select distinct vaers_id, symptom1 symptom
 from dataset1.symptoms2014
+where symptom1 is not null
 union distinct
 select distinct vaers_id, symptom2 symptom
 from dataset1.symptoms2014
+where symptom2 is not null
 union distinct
 select distinct vaers_id, symptom1 symptom
 from dataset1.symptoms2015
@@ -124,9 +143,11 @@ from dataset1.symptoms2015
 union distinct
 select distinct vaers_id, symptom1 symptom
 from dataset1.symptoms2016
+where symptom1 is not null
 union distinct
 select distinct vaers_id, symptom2 symptom
 from dataset1.symptoms2016
+where symptom2 is not null
 
 --
 --Vaccines 2006-2016
@@ -141,47 +162,47 @@ order by serialid
 
 --UNION QUERY
 
-select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route
+select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route, "2006" as year
 from dataset1.vaccine2006
 where vax_route is not null and vax_name is not null and vax_manu is not null
 union distinct
-select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route
+select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route, "2007" as year
 from dataset1.vaccine2007
 where vax_route is not null and vax_name is not null and vax_manu is not null
 union distinct
-select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route
+select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route, "2008" as year
 from dataset1.vaccine2008
 where vax_route is not null and vax_name is not null and vax_manu is not null
 union distinct
-select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route
+select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route, "2009" as year
 from dataset1.vaccine2009
 where vax_route is not null and vax_name is not null and vax_manu is not null
 union distinct
-select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route
+select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route, "2010" as year
 from dataset1.vaccine2010
 where vax_route is not null and vax_name is not null and vax_manu is not null
 union distinct
-select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route
+select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route, "2011" as year
 from dataset1.vaccine2011
 where vax_route is not null and vax_name is not null and vax_manu is not null
 union distinct
-select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route
+select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route, "2012" as year
 from dataset1.vaccine2012
 where vax_route is not null and vax_name is not null and vax_manu is not null
 union distinct
-select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route
+select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route, "2013" as year
 from dataset1.vaccine2013
 where vax_route is not null and vax_name is not null and vax_manu is not null
 union distinct
-select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route
+select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route, "2014" as year
 from dataset1.vaccine2014
 where vax_route is not null and vax_name is not null and vax_manu is not null
 union distinct
-select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route
+select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route, "2015" as year
 from dataset1.vaccine2015
 where vax_route is not null and vax_name is not null and vax_manu is not null
 union distinct
-select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route
+select distinct vaers_id, vax_name, vax_type, vax_manu, vax_route, "2016" as year
 from dataset1.vaccine2016
 where vax_route is not null and vax_name is not null and vax_manu is not null
 
