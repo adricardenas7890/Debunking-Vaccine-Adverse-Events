9721b5bb92a79ec77e96e7a3be9c42c0ea6b9a83
sofiashubert <32207478+sofiashubert@users.noreply.github.com>
2019-03-04 21:06:06 -0600

    New pardo.py with random number PK and fixed shema

--- pardo.py
+++ pardo.py
@@ -1,22 +1,27 @@
 import os
 import apache_beam as beam
+import random
 from apache_beam.io import ReadFromText
 from apache_beam.io import WriteToText
 
+
 # DoFn performs on each element in the input PCollection.
-# Adds primary key to table 
+# Adds primary key to table
 class VaccineCountFn(beam.DoFn):
-  count = 1
   def process(self, element):
     record = element
-    vax_id = self.count
+    vax_id = round(random.random() * 10000000)
     vaers_id= record.get('vaers_id')
     vax_name = record.get('vax_name')
     vax_type = record.get('vax_type')
     vax_manu = record.get('vax_manu')
     vax_route = record.get('vax_route')
     year = record.get('year')
-    count += 1
+
+    record = {'vax_id': vax_id, 'vaers_id': vaers_id, 'vax_name': vax_name,
+              'vax_type': vax_type, 'vax_manu': vax_manu, 'vax_route': vax_route,
+              'year': year}
+    return [record]
     
 
 class MakeRecordFn(beam.DoFn):
@@ -39,7 +44,7 @@ opts = beam.pipeline.PipelineOptions(flags=[], **options)
 # Create a Pipeline using a local runner for execution.
 with beam.Pipeline('DirectRunner', options=opts) as p:
 
-    query_results = p | 'Read from BigQuery' >> beam.io.Read(beam.io.BigQuerySource(query='SELECT * FROM dataset1clean.vaccine'))
+    query_results = p | 'Read from BigQuery' >> beam.io.Read(beam.io.BigQuerySource(query='SELECT * FROM dataset1clean.vaccine LIMIT 100'))
 
     # write PCollection to log file
     query_results | 'Write to log 1' >> WriteToText('input.txt')
@@ -55,7 +60,7 @@ with beam.Pipeline('DirectRunner', options=opts) as p:
     vaxid_out_pcoll = vaxid_in_pcoll | 'Make BQ Record' >> beam.ParDo(MakeRecordFn())
     
     qualified_table_name = PROJECT_ID + ':dataset1clean.vaccine_PK'
-    table_schema = 'name:STRING,count:INTEGER'
+    table_schema = 'vax_id:INTEGER, vaers_id:INTEGER, vax_name:STRING, vax_type:STRING, vax_manu:STRING, vax_route:STRING, year:INTEGER'
     
     vaxid_out_pcoll | 'Write to BigQuery' >> beam.io.Write(beam.io.BigQuerySink(qualified_table_name, 
                                                     schema=table_schema,  
