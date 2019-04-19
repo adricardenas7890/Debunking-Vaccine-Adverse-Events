34f247c97ee86087e2d02a5502db137a686a65e1
Adriana Cardenas <adri.cardenas@utexas.edu>
2019-04-19 15:41:27 -0500

    changes singl.py

--- transform_personalinfo_single.py
+++ transform_personalinfo_single.py
@@ -15,18 +15,18 @@ class changeNullsFn(beam.DoFn):
             state = "U"
         hospitalization = record.get('hospitalization')
         if hospitalization== None:
-            hospitalization = "false"
+            hospitalization = str("false")
         disabled = record.get('disabled')
         if disabled == None:
-            disabled = "false"
+            disabled = str("false")
         age = record.get('age')
         sex = record.get('sex')
         died = record.get('died')
         if died == None:
-            died = "false"
+            died = str("false")
         recovered = record.get('recovered')
         if recovered == None:   
-            recovered = "U"
+            recovered = str("U")
         year = record.get('year')
 
         record = {'vaers_id': vaers_id, 'state':state, 'hospitalization':hospitalization,
@@ -70,7 +70,7 @@ with beam.Pipeline('DirectRunner', options=opts) as p:
     changeNulls_out_pcoll = changeNulls_in_pcoll | 'Make BQ Record' >> beam.ParDo(MakeRecordFn())
     
     qualified_table_name = PROJECT_ID + ':dataset2.primaryinfo_2'
-    table_schema = 'vaers_id:STRING, state:STRING, hospitalization:STRING, disabled:STRING, age:FLOAT, sex:STRING, died:STRING, recovered:STRING, year:STRING'
+    table_schema = 'vaers_id:STRING,state:STRING,hospitalization:STRING,disabled:STRING,age:FLOAT,sex:STRING,died:STRING,recovered:STRING,year:STRING'
     
     changeNulls_out_pcoll | 'Write to BigQuery' >> beam.io.Write(beam.io.BigQuerySink(qualified_table_name, 
                                                     schema=table_schema,  
