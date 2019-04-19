d882fe6c373f4eb2660e99a6926a7ce97f5a377a
Adriana Cardenas <adri.cardenas@utexas.edu>
2019-04-19 17:04:06 -0500

    changes cluster.py

--- transform_personalinfo_cluster.py
+++ transform_personalinfo_cluster.py
@@ -7,43 +7,48 @@ from apache_beam.io import ReadFromText
 from apache_beam.io import WriteToText
 
 # DoFn performs on each element in the input PCollection.
-# change all null values
+# Changes null to values
 class changeNullsFn(beam.DoFn):
     def process(self, element):
         record = element
         vaers_id = record.get('vaers_id')
+        vaers_id = str(vaers_id)
         state = record.get('state')
         if state == None:
             state = str("U")
+        else:
+            state = str(state)
         hospitalization = record.get('hospitalization')
         if hospitalization == None:
-            hospitalization = False
+            hospitalization = str("false")
+        else:
+            hospitalization = str(hospitalization)
         disabled = record.get('disabled')
         if disabled == None:
-            disabled = False
+            disabled = str("false")
+        else:
+            disabled = str(disabled)
         age = record.get('age')
+        age = str(age)
         sex = record.get('sex')
         died = record.get('died')
         if died == None:
-            died = False
+            died = str("false")
+        else:
+            died = str(died)
         recovered = record.get('recovered')
         if recovered == None:
-            recovered = "U"
+            recovered = str("U")
+        else:
+            recovered = str(recovered)
         year = record.get('year')
+        year = str(year)
 
         record = {'vaers_id': vaers_id, 'state':state, 'hospitalization':hospitalization,
                   'disabled':disabled, 'age':age, 'sex':sex, 'died':died,
                   'recovered':recovered, 'year':year}
 
         return [record]
-
-class MakeRecordFn(beam.DoFn):
-  def process(self, element):
-    vaers_id, state, hospitalization, disabled, age, sex, died, recovered, year = element
-    record = {'vaers_id': vaers_id, 'state':state, 'hospitalization':hospitalization,
-              'disabled':disabled, 'age':age, 'sex':sex, 'died':died,
-              'recovered':recovered, 'year':year}
-    return [record] 
         
 PROJECT_ID = os.environ['PROJECT_ID']
 BUCKET = os.environ['BUCKET']
@@ -67,7 +72,7 @@ with beam.Pipeline('DataflowRunner', options=opts) as p:
 # Create a Pipeline using a local runner for execution.
 with beam.Pipeline('DirectRunner', options=opts) as p:
 
-    query_results = p | 'Read from BigQuery' >> beam.io.Read(beam.io.BigQuerySource(query='SELECT * FROM dataset2.primaryinfo LIMIT 100'))
+    query_results = p | 'Read from BigQuery' >> beam.io.Read(beam.io.BigQuerySource(query='SELECT * FROM dataset2.primaryinfo'))
 
     # write PCollection to log file
     query_results | 'Write to log 1' >> WriteToText('input.txt')
@@ -80,10 +85,10 @@ with beam.Pipeline('DirectRunner', options=opts) as p:
     
     # make BQ records, when writing from Beam to BQ, BQ expects a dictionary
   
-    changeNulls_out_pcoll = changeNulls_in_pcoll | 'Make BQ Record' >> beam.ParDo(MakeRecordFn())
+    #changeNulls_out_pcoll = changeNulls_in_pcoll | 'Make BQ Record' >> beam.ParDo(MakeRecordFn())
     
     qualified_table_name = PROJECT_ID + ':dataset2.primaryinfo_3'
-    table_schema = 'vaers_id:STRING, state:STRING, hospitalization:BOOLEAN, disabled:BOOLEAN, age:FLOAT, sex:STRING, died:BOOLEAN, recovered:STRING, year:STRING'
+    table_schema = 'vaers_id:STRING,state:STRING,hospitalization:STRING,disabled:STRING,age:STRING,sex:STRING,died:STRING,recovered:STRING,year:STRING'
     
     changeNulls_out_pcoll | 'Write to BigQuery' >> beam.io.Write(beam.io.BigQuerySink(qualified_table_name, 
                                                     schema=table_schema,  
